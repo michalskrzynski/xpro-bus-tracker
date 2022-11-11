98620cdade7315b35a467eaaa6620b2f614edabb
michalskrzynski <michal.skrzynski@gmail.com>
2022-11-11 13:10:20 +0100

    accomplish v1

---
+++ README.md
@@ -0,0 +1,20 @@
+# Real Time Bus Tracker
+
+This project is one of assignments from MIT xPro MERN Stack course. 
+
+
+# Project description
+
+This project displays the real time location of a bus. The location is loaded from the public database MBTA.
+
+## How to run 
+
+Just download the project and start **index.html** file.
+
+## Roadmap of future improvements
+
+It would be nice to add the functionality to switch bus lines. Later it'd be great to suggest the shortest bus route to a desired destination.
+
+## License
+
+The project is under MIT license.
--- mapanimation.js
+++ mapanimation.js
@@ -1,20 +1,13 @@
-// This array contains the coordinates for all bus stops between MIT and Harvard
-const busStops = [
-  [-71.093729, 42.359244],
-  [-71.094915, 42.360175],
-  [-71.0958, 42.360698],
-  [-71.099558, 42.362953],
-  [-71.103476, 42.365248],
-  [-71.106067, 42.366806],
-  [-71.108717, 42.368355],
-  [-71.110799, 42.369192],
-  [-71.113095, 42.370218],
-  [-71.115476, 42.372085],
-  [-71.117585, 42.373016],
-  [-71.118625, 42.374863],
-];
+let stop = false;
 
-// TODO: add your own access token
+const colors = ['black','white','red','lime','blue','yellow','cyan','magenta',
+              'silver','grey','maroon','olive','green','purple','teal','navy'];
+let totalMarkers = 0;
+
+
+const apiUrl = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+// its a bad practice to include the accessToken within the public GIT repository, however for the 
+// purpose of xPro course, where the project could be easily accessible, I will leave it that way
 mapboxgl.accessToken = 'pk.eyJ1IjoibWljaGFsc2tyenluc2tpIiwiYSI6ImNqOG9wZnFoMjA2OW4ycXVtbG16c2VtM24ifQ.qMYt5Ps5EBb6Na88-9P-bA';
 
 // This is the map instance
@@ -22,11 +15,74 @@ let map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
   center: [-71.104081, 42.365554],
-  zoom: 14,
+  zoom: 12,
 });
 
+let busData = null;
+
+
+async function getBusLocations() {
+  const response = await fetch( apiUrl );
+  const json = await response.json();
+  const newBusData = {};
+
+  for( let i =0; i < json.data.length; i++ ) {
+    const bd = new BusData( json.data[i] );
+    newBusData[ bd.id ] = bd;
+  }
+
+  return newBusData;
+}
+
+async function run() {
+  if( stop ) return;
+
+  console.log( `Running: ${new Date()}`);
+
+  const newBusData  = await getBusLocations();
+  if( busData === null ) busData = newBusData;
+  else {
+    updateBusData( newBusData );
+  }
+  addMissingMarkers();
+  moveAccordingly();
+
+  setTimeout( run, 15000 );
+}
+
+
+function updateBusData( newBusData ) {
+  Object.values( newBusData ).forEach( bd => {
+    if( busData[ bd.id ] == null ) {
+      busData[ bd.id ] = bd;
+    } 
+    else {
+      busData[ bd.id ].update( bd );
+    }
+  });
+}
+
+function addMissingMarkers() {
+  let busDataValues = Object.values( busData )
+  busDataValues.forEach( (bd) => {
+    if( bd.marker === null ) {
+      bd.marker = new mapboxgl.Marker( { "color": colors[totalMarkers % busDataValues.length] } );
+      bd.marker.setLngLat( [bd.longitude, bd.latitude]).addTo(map);
+      totalMarkers++;
+    } 
+  });
+};
+
+function moveAccordingly() {
+  Object.values( busData ).forEach( (bd) => {
+    bd.marker.setLngLat( [bd.longitude, bd.latitude]);
+  });
+}
+
+
+
 // TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
-let marker = new mapboxgl.Marker().setLngLat(busStops[0]).addTo(map);
+//let marker = new mapboxgl.Marker().setLngLat(busStops[0]).addTo(map);
 
 // counter here represents the index of the current bus stop
 let counter = 0;
@@ -39,7 +95,39 @@ function move() {
   if( counter <= busStops.length ) setTimeout( move, 1000 );
 }
 
-// Do not edit code past this point
-if (typeof module !== 'undefined') {
-  module.exports = { move };
+
+class BusData {
+  
+  #id;
+  #currentStatus;
+  #currentStopSequence;
+  #latitude;
+  #longitude;
+  #marker = null;
+
+
+  constructor( jsonInput ) {
+    this.#id = jsonInput.id;
+    this.#currentStatus = jsonInput.attributes.current_status;
+    this.#currentStopSequence = jsonInput.attributes.current_stop_sequence;
+    this.#latitude = jsonInput.attributes.latitude;
+    this.#longitude = jsonInput.attributes.longitude;
+  }
+
+  update( other ) {
+    this.#currentStatus = other.currentStatus;
+    this.#currentStopSequence = other.currentStatus;
+    this.#latitude = other.latitude;
+    this.#longitude = other.longitude;
+  }
+
+  get id() { return this.#id }
+  get currentStatus() { return this.#currentStatus }
+  get currentStopSequence() { return this.#currentStopSequence }
+  get latitude() { return this.#latitude }
+  get longitude() { return this.#longitude }
+  get marker() { return this.#marker }
+
+  set marker( m ) { this.#marker = m }
+
 }
